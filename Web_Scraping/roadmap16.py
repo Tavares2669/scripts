74433e2c56c1e6ffc3e89567b77ebe3b40227ccc
Moises <moisesdalcol@gmail.com>
2026-06-25 16:41:37 +0100

    scraping em 3 sites pelo titulo e preco, data cleaning e comparacao thresold para adquirir livros mais com o preco mais baixo

---
+++ Web_Scraping/roadmap16.py
@@ -0,0 +1,37 @@
+from playwright.async_api import async_playwright
+import asyncio
+
+async def extrair_dados_livro(page, threshold):
+    titulo = await page.locator("div > h1").inner_text()
+    preco = await page.locator(".price_color").first.inner_text()
+    preco_num = float(preco.replace("£",""))
+    
+    dados_livro = {
+        "title": titulo,
+        "price": preco_num,
+        "cheap": preco_num < threshold
+    }
+    return dados_livro
+async def main():
+    async with async_playwright() as p:
+        lista_livros = [
+            "https://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html",
+            "https://books.toscrape.com/catalogue/tipping-the-velvet_999/index.html",
+            "https://books.toscrape.com/catalogue/soumission_998/index.html"
+        ]
+        threshold =  50.5
+        browser = await p.chromium.launch(headless=False)
+        page = await browser.new_page()
+        for url in lista_livros:
+            await page.goto(url)
+            print("A extrair dados")
+
+            esta_barato = await extrair_dados_livro(page,threshold)
+            if esta_barato["cheap"] == True:
+                print(f"Dentro da margem: Livro {esta_barato['title']}")
+                print(f"Preço atual: £{esta_barato['price']} (O limite era £{threshold})")
+            else:
+                print(f"Demasiado caro: Livro {esta_barato['title']}")
+                print(f"Preço atual: £{esta_barato['price']} (Ultrapassa o limite de £{threshold})")
+            await asyncio.sleep(3)
+asyncio.run(main())
